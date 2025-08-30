cba229100da04e3b7033f69bf9e32981be095e65
OmShah74 <omamitshah@gmail.com>
2025-08-30 10:48:48 +0530

    Change4

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Om Shah | Software Developer Portfolio</title>
+        <title>Om Shah</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* These links are for the icons from the original HTML */}
+        <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </Head>
